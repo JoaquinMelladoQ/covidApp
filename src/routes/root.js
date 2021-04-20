df4ce58bf9e8be7c2b86972bbb726d07949214de
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-04-19 22:23:32 -0400

    Building stack navigation

---
+++ src/routes/root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack{ createStackNavigator }';
+
+const RootStack = createStackNavigator();
+
+const RootNavigation = () => (
+  <NavigationContainer>
+  </NavigationContainer>
+)
+
+export default RootNavigation
