b47e33f455ef8ab9acde8bcce66b0aed999366c6
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-04-26 17:50:22 -0400

    14:52 2cond part class 9

--- src/screen/Hooks/UseCallback.js
+++ src/screen/Hooks/UseCallback.js
@@ -1,5 +1,5 @@
-import React, { useCallback } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,12 +7,50 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  box: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
 })
 
+const randomColor = () => '#' + ((Math.random() * 0xffffff) << 0).toString(16)
+
+const Button = ({ onPress, title }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{ backgroundColor: randomColor(), padding: 5, }}
+  >
+    <Text style={{ fontSize: 20 }}>{title}</Text>
+  </TouchableOpacity>
+)
+
+const functions = new Set()
+
 const UseCallback = () => {
+  const [delta, setDelta] = useState(1);
+  const [incremental, setIncremental] = useState(0);
+
+  const incrementalDelta = () => setDelta((delta) => delta + 1)
+  const incremento = () => setIncremental((incremental) => incremental + delta)
+  
+  functions.add(incrementalDelta)
+  functions.add(incremento)
+
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Delta es {delta}</Text>
+      <Text>Contador es incremental {incremental}</Text>
+      <View style={styles.box}>
+        <Button 
+          title="Incrementar delta" 
+          onPress={incrementalDelta}
+        />
+        <Button 
+          title="Incrementar Counter" 
+          onPress={incremento}
+        />
+      </View>
+      <Text>Nuevas funciones agregadas al set: {functions.size - 2}</Text>
     </View>
   )
 }
