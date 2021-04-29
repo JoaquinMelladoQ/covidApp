80be8f03ded0cdcccc0a04042601b97f30c6489f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-04-29 00:25:32 -0400

    Min 50:18 before touchableopacity

--- src/screen/Login/LoginScreen.js
+++ src/screen/Login/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
 import colors from '../../config/colors.js'
 
 const styles = StyleSheet.create({
@@ -13,16 +13,47 @@ const styles = StyleSheet.create({
     fontSize: 40,
     color: colors.white,
   },
+  inputsContainer: {
+    paddingTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: '80%',
+    paddingTop: 10,
+    borderWidth: 1,
+    borderColor: colors.black,
+    marginVertical: 10,
+    borderRadius: 8,
+    fontSize: 19,
+  },
 })
 
 const Login = () => {
-  const [user, updateUserName] = useState('');
+  const [userName, updateUserName] = useState('');
   const [userPassword, updateUserPassword] = useState(''); 
 
   return (
     <>
       <View style={styles.header}>
         <Text style={styles.headerText}>Login</Text>
+      </View>
+      <View style={styles.inputsContainer}>
+        <TextInput 
+          placeholder="Usuario"
+          autoCapitalize="none"
+          value={userName}
+          onChangeText={(name) => updateUserName(name)}
+          style={styles.textInput}
+        />
+        <TextInput 
+          placeholder="Password"
+          autoCapitalize="none"
+          value={userPassword}
+          secureTextEntry
+          onChangeText={(password) => updateUserPassword(password)}
+          style={styles.textInput}
+        />
       </View>
     </>
   )
